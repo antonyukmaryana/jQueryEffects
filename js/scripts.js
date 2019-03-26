6ff0ca6f277cac77a14d75ca2a3743977ea0239c
Maryana Antonyuk and Stephen Trewick <maryana@>
2019-03-26 16:09:13 -0700

    add talks to webpage,append buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+  $("button#hello").click(function() {
+      $("ul#user").append("<li>Hello!</li>");
+      $("ul#webpage").append("<li>Why hello there!</li>");
+    });
+
+    $("button#goodbye").click(function() {
+      $("ul#user").append("<li>Goodbye!</li>");
+      $("ul#webpage").append("<li>Goodbye, dear user!</li>");
+    });
+
+    $("button#stop").click(function() {
+      $("ul#user").append("<li>Stop copying me!</li>");
+      $("ul#webpage").append("<li>Pardon me. I meant no offense.</li>");
+    });
 });
